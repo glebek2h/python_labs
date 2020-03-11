292ec574c639087abbf461c285a791eb9cda0a5f
Kazachynski, Gleb <gleb.kazachynski@wolterskluwer.com>
2020-03-11 23:17:51 +0300

    minor

--- l3/3.1.py
+++ l3/3.1.py
@@ -1,14 +1,9 @@
 import numpy as np
+from scipy.linalg import expm
 
-a = [
-    [1, 0, 0, 0, 9, 0, 0],
-    [7, 3, 0, 0, 9, 0, 0],
-    [0, 7, 5, 0, 9, 0, 0],
-    [0, 0, 7, 7, 9, 0, 0],
-    [0, 0, 0, 9, 9, 0, 0],
-    [0, 0, 0, 0, 9, 11, 0],
-    [0, 0, 0, 0, 9, 7, 13],
-]
-e = np.eye(7, dtype=int)
-exp_a = np.exp(a)
-print exp_a
+A = np.diagflat(np.arange(1, 15, 2, dtype=int), 0) + np.diag(np.full(6, 7, dtype=int), -1)
+A[:, 4] = np.full(7, 9)
+A[4][3] = 9
+
+R = np.vstack((np.hstack((A, np.eye(7))), np.hstack((np.exp(A), expm(A)))))
+np.savetxt('C.txt', R)
--- l4/4.3.py
+++ l4/4.3.py
@@ -1,3 +1,5 @@
+# python 3
+
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
